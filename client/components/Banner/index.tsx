c76c9af3dffc164b9a8e9333203fd4123d7e770e
Amir Farhadi <frhadyamyr283@gmail.com>
2024-10-02 12:43:50 +0330

    Clean Banner Code

--- client/components/Banner/index.tsx
+++ client/components/Banner/index.tsx
@@ -1,20 +1,19 @@
-"use client";
-
-import * as React from "react";
-import { styled } from "@mui/system";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid2";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#fff",
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles("dark", {
-    backgroundColor: "#1A2027",
-  }),
-}));
+// Reusable box shadow style
+const boxShadowStyle = {
+  boxShadow: "0 0 3px 0 rgba(0, 0, 0, 0.45)",
+  height: "100%",
+  borderRadius: 1,
+};
+
+// Reusable height configuration
+const heightConfig = {
+  grid1: { xs: 1 / 6, sm: 2 / 3 },
+  grid2: { xs: "calc(33.3% + 16.25px)", sm: 2 / 3 },
+  grid3: { xs: 1 / 6, sm: 1 / 3 },
+};
 
 export default function Banner() {
   return (
@@ -22,53 +21,40 @@ export default function Banner() {
       sx={{
         flexGrow: 1,
         height: {
-          xs: "calc( 100svh - 118px )",
-          sm: "calc( 100svh - 86px )",
+          xs: "calc(100svh - 118px)",
+          sm: "calc(100svh - 86px)",
         },
         padding: { xs: 1, sm: 2 },
       }}
     >
       <Grid container spacing={{ xs: 1, sm: 2 }} sx={{ height: "100%" }}>
-        <Grid
-          size={{ xs: 12, sm: 8 }}
-          sx={{ height: { xs: 1 / 6, sm: 2 / 3 } }}
-        >
-          <Item sx={{ height: "100%" }}></Item>
+        <Grid size={{ xs: 12, sm: 8 }} sx={{ height: heightConfig.grid1 }}>
+          <Box sx={boxShadowStyle}></Box>
         </Grid>
+
         <Grid
           container
           size={{ xs: 12, sm: 4 }}
           spacing={{ xs: 1, sm: 2 }}
           direction="column"
-          sx={{
-            height: { xs: "calc(33.3% + 16.25px)", sm: 2 / 3 },
-          }}
+          sx={{ height: heightConfig.grid2 }}
         >
-          <Grid size={12} sx={{ flex: 1 }}>
-            <Item sx={{ height: "100%" }}></Item>
+          {[...Array(2)].map((_, idx) => (
+            <Grid key={idx} size={12} sx={{ flex: 1 }}>
+              <Box sx={boxShadowStyle}></Box>
+            </Grid>
+          ))}
+        </Grid>
+
+        {[...Array(3)].map((_, idx) => (
+          <Grid
+            key={idx}
+            size={{ xs: 12, sm: 4 }}
+            sx={{ height: heightConfig.grid3 }}
+          >
+            <Box sx={boxShadowStyle}></Box>
           </Grid>
-          <Grid size={12} sx={{ flex: 1 }}>
-            <Item sx={{ height: "100%" }}></Item>
-          </Grid>
-        </Grid>
-        <Grid
-          size={{ xs: 12, sm: 4 }}
-          sx={{ height: { xs: 1 / 6, sm: 1 / 3 } }}
-        >
-          <Item sx={{ height: "100%" }}></Item>
-        </Grid>
-        <Grid
-          size={{ xs: 12, sm: 4 }}
-          sx={{ height: { xs: 1 / 6, sm: 1 / 3 } }}
-        >
-          <Item sx={{ height: "100%" }}></Item>
-        </Grid>
-        <Grid
-          size={{ xs: 12, sm: 4 }}
-          sx={{ height: { xs: 1 / 6, sm: 1 / 3 } }}
-        >
-          <Item sx={{ height: "100%" }}></Item>
-        </Grid>
+        ))}
       </Grid>
     </Box>
   );
