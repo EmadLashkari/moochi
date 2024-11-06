03bff4a78913940f392d874a8af8f38edfcb067f
Amir Farhadi <frhadyamyr283@gmail.com>
2024-11-06 23:45:41 +0330

    Start Footer

--- client/components/Footer/index.tsx
+++ client/components/Footer/index.tsx
@@ -1,14 +1,13 @@
 import { BoxColumn, BoxRow } from "@/utils/custom";
-import { Box } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 function Footer() {
   return (
     <>
       <BoxRow
         sx={{
-          width: "calc100svw",
-          height: "500px",
-          minHeight: "500px",
+          width: "100svw",
+          height: "350px",
           p: 2,
           gap: 2,
           bgcolor: "primary.main",
@@ -16,16 +15,46 @@ function Footer() {
             xs: "column",
             md: "row",
           },
+          flex: 2,
         }}
       >
         {/* Right */}
-        <BoxColumn>
-          <BoxColumn></BoxColumn>
-          <Box></Box>
+        <BoxColumn sx={{ flex: 1, height: 1 }}>
+          {/* Right-Top */}
+          <BoxColumn sx={{ flexShrink: 1, height: 7 / 10, gap: 0, padding: 4 }}>
+            <BoxColumn sx={{ flex: 1 / 4 }}>
+              <Typography variant="h4" color="background.default">
+                موچی چیه؟
+              </Typography>
+            </BoxColumn>
+            <BoxColumn sx={{ flex: 1 / 4 }}>
+              <Typography variant="body1" color="background.default">
+                با یه کلیک، در هر آرایشگاهی که دوست داری موهاتو کوتاه کن.
+              </Typography>
+            </BoxColumn>
+            <BoxColumn sx={{ flex: 1 / 4 }}>
+              <Typography variant="body1" color="background.default">
+                شماره تماست رو داخل این باکس بنویس، در اولین فرصت با شما تماس
+                می‌گیریم.
+              </Typography>
+            </BoxColumn>
+
+            <BoxRow sx={{ flex: 1 / 4 }}>
+              <TextField
+                fullWidth
+                size="medium"
+                variant="outlined"
+                sx={{ bgcolor: "background.default" }}
+              />
+              <Button></Button>
+            </BoxRow>
+          </BoxColumn>
+          {/* Right-Bottom */}
+          <Box sx={{ flexShrink: 1, height: 3 / 10 }}>hi</Box>
         </BoxColumn>
         {/* Left */}
-        <BoxRow>
-          <BoxColumn></BoxColumn>
+        <BoxRow sx={{ flex: 1, height: 1 }}>
+          <BoxColumn>h</BoxColumn>
           <Box></Box>
         </BoxRow>
       </BoxRow>
