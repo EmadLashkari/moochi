005fe33de7a74fb2a96f0b9b937394fe0e53d9f1
Amir Farhadi <frhadyamyr283@gmail.com>
2024-11-12 23:09:18 +0330

    reset coinfig.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
-  images: {
-    localPatterns: [
-      {
-        pathname: "/assets/images/**",
-        search: "",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
